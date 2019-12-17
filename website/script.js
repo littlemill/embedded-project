4644268f5ed340582073ef37aec3703605beab83
kanjeranian <oilscius@gmail.com>
2019-12-17 14:56:47 +0700

    html by mill

--- website/script.js
+++ website/script.js
@@ -12,15 +12,15 @@ var microgear = Microgear.create({
 });
 
 microgear.connect(APPID);
-
 function falling(logic) {
   if (logic == 1) {
+    console.log('logic1 = on');
     microgear.publish('/fall/command', 'ON');
-    document.getElementById('status_fall_btn').innerHTML = 'Status : ON';
   } else if (logic == 0) {
+    console.log('logic0 = off');
     microgear.publish('/fall/command', 'OFF');
-    document.getElementById('status_fall_btn').innerHTML = 'Status : ON';
   } else if (logic == 2) {
+    console.log('logic2 = reset');
     microgear.publish('/fall/command', 'RESET');
   }
 }
@@ -29,11 +29,9 @@ function goOut(logic) {
   if (logic == 1) {
     console.log('logic1 = on');
     microgear.publish('/out-of-range/command', 'ON');
-    document.getElementById('status_range_btn').innerHTML = 'Status : ON';
   } else if (logic == 0) {
     console.log('logic0 = off');
     microgear.publish('/out-of-range/command', 'OFF');
-    document.getElementById('status_range_btn').innerHTML = 'Status : OFF';
   } else if (logic == 2) {
     console.log('logic2 = reset');
     microgear.publish('/out-of-range/command', 'RESET');
@@ -44,27 +42,37 @@ microgear.on('connected', function() {
   console.log('NETPIE Connected');
   microgear.setAlias(ALIAS);
   document.getElementById('connected_NETPIE').innerHTML =
-    'netpie status : connected !';
+    'Netpie status : connected !';
 });
 
 microgear.on('message', function(topic, data) {
   if (topic == '/fall') {
     if (data == 'ok') {
-      document.getElementById('status_fall').innerHTML = 'Every is OKAY :)';
+      document.getElementById('status_fall').innerHTML = 'Everything is OKAY :)';
     } else if (data == 'fall') {
-      document.getElementById('status_fall').innerHTML = 'Falling!!!!!!!!!';
+      document.getElementById('status_fall').innerHTML = 'The patient is falling!!!!!!!!!';
     }
   } else if (topic == '/range') {
     if (data == 'in') {
-      document.getElementById('status_fall').innerHTML = 'I can see you';
+      document.getElementById('status_fall').innerHTML = "Patient's location is available";
     } else if (data == 'out') {
-      document.getElementById('status_fall').innerHTML = 'where are youuu';
+      document.getElementById('status_fall').innerHTML = "Patient's location is unavailable";
     }
   } else if (topic == '/board') {
     if (data == 'fine') {
       document.getElementById('status_fall').innerHTML = 'use me XD';
     } else if (data == 'error') {
       document.getElementById('status_fall').innerHTML = 'fix me XP';
+    }
+  } else if(topic == "/status/command") {
+    if(data=="fall_on"){
+      document.getElementById('status_fall_btn').innerHTML = "Status : ON";
+    }else if(data=="fall_off"){
+      document.getElementById('status_fall_btn').innerHTML = "Status : OFF";
+    }else if(data=="out_on"){
+      document.getElementById('status_range_btn').innerHTML = "Status : ON";
+    }else if(data=="out_off"){
+      document.getElementById('status_range_btn').innerHTML = "Status : OFF";
     }
   }
 });
